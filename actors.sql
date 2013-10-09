27e1e413d8032cfdf69f7758f25e0e1c0f402513
Josh-Dykstra <joshuajdykstra@gmail.com>
2013-10-08 22:58:17 -0700

    Part B: MySQL warmup
    * Added actors.sql file with mysql exercises

---
+++ actors.sql
@@ -0,0 +1,24 @@
+-- Create the Actors table with required schema --
+
+CREATE TABLE `Actors` (
+`Name` VARCHAR(40),
+`Movie` VARCHAR(80),
+`Year` INTEGER,
+`Role` VARCHAR(40)
+);
+
+-- Load the table with the actors.csv file --
+
+LOAD DATA LOCAL INFILE '~/ebay-data/actors.csv' 
+INTO TABLE `Actors`
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+-- Query for all the actors in 'Die Another Day' --
+
+SELECT `name`
+FROM `Actors`
+WHERE `movie` = 'Die Another Day';
+
+-- Drop the Actors table --
+
+DROP TABLE `Actors`;
